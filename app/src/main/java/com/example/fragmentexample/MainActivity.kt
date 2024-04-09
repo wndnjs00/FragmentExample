2a31202dd97a56563e7a8862b8c35631ecf7d5d6
jeonjuwon <juwonjeon852@gmail.com>
2024-04-09 17:38:24 +0900

    [FEAT]프레그먼트 구현

--- app/src/main/java/com/example/fragmentexample/MainActivity.kt
+++ app/src/main/java/com/example/fragmentexample/MainActivity.kt
@@ -1,16 +1,43 @@
 package com.example.fragmentexample
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.fragmentexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        // 뷰바인딩 사용
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        binding.apply {
+            fragment1Btn.setOnClickListener{
+                setFragment(FirstFragment())
+            }
+            fragment2Btn.setOnClickListener {
+                setFragment(SecondFragment())
+            }
+        }
+
+        // MainActivity실행되자마자 나타날 프레그먼트
+        setFragment(FirstFragment())
     }
+
+    private fun setFragment(frag : Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, frag)
+            .commit()
+    }
+
+
 }
